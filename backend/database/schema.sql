48e956db1b12938bfde09601a3f711c85562bfc6
YounesSefiani <tokyotweener21@gmail.com>
2025-01-20 16:38:25 +0100

    Modification de la table des seasons(ajout de clé étrangère pour la lier à la table des séries) + Création de la table des episodes, du casting des films et des séries, de la table des episodes, personnalités et users

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -38,23 +38,76 @@ CREATE TABLE series (
   streaming ENUM('Netflix', 'Disney +', 'Amazon Prime Vidéo', 'Paramount +', 'Apple TV', 'Salto', 'OCS', 'Canal +') NULL
   );
 
-  CREATE TABLE seasons (
+ CREATE TABLE seasons (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  serie_id INT NOT NULL,
   season_number INT NOT NULL,
   poster VARCHAR(255),
   first_episode_date DATE,
   last_episode_date DATE,
-  trailer VARCHAR(255),
   synopsis TEXT,
-  episodes INT
+  episodes INT,
+  FOREIGN KEY (serie_id) REFERENCES series(id) ON DELETE CASCADE,
+  CONSTRAINT unique_season_per_series UNIQUE(serie_id, season_number)
 );
 
 CREATE TABLE episodes (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  serie_id INT NOT NULL,
+  season_id INT NOT NULL,
   episode_number INT NOT NULL,
   title VARCHAR(255),
-  imgEpisode VARCHAR(255),
+  image VARCHAR(255),
   release_date DATE,
-  synopsis TEXT
+  synopsis TEXT,
+  FOREIGN KEY (serie_id) REFERENCES series(id) ON DELETE CASCADE,
+  FOREIGN KEY (season_id) REFERENCES seasons(id) ON DELETE CASCADE,
+  CONSTRAINT unique_episode_per_season_and_series UNIQUE(serie_id, season_id, episode_number)
 );
 
+CREATE TABLE personalities (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  fullname VARCHAR(255) NOT NULL,
+  image_src VARCHAR(255) NOT NULL,
+  birthdate DATE NOT NULL,
+  deathdate DATE,
+  origin VARCHAR(255) NOT NULL,
+  bio TEXT,
+  profession VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE movieCasting (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  movie_id INT NOT NULL,
+  personality_id INT NOT NULL,
+  side ENUM("Acting", "Realisation") NOT NULL,
+  role VARCHAR(255) NOT NULL,
+  FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE,
+  FOREIGN KEY (personality_id) REFERENCES personalities(id) ON DELETE CASCADE,
+  CONSTRAINT unique_personality_movie UNIQUE(movie_id, personality_id)
+);
+
+CREATE TABLE serieCasting (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  serie_id INT,
+  personality_id INT,
+  side ENUM("Acting", "Realisation") NOT NULL,
+  role VARCHAR(255) NOT NULL,
+  presence VARCHAR(255) NOT NULL,
+  FOREIGN KEY (serie_id) REFERENCES series(id) ON DELETE CASCADE,
+  FOREIGN KEY (personality_id) REFERENCES personalities(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  pseudo VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  mail VARCHAR(255) NOT NULL UNIQUE,
+  birthdate DATE NOT NULL,
+  avatar VARCHAR(255),
+  role ENUM("User", "Admin") DEFAULT "User"
+);
+
+INSERT INTO users (pseudo, password, mail, birthdate, avatar)
+VALUES ('admin', 'admin', 'H4QvM@example.com', '2000-01-01', 'avatar.jpg');
